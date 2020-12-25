f37cf9ab6dfce4202ef56418b878dd71142c8b6c
goldsnake <goldsnake@RT-AC68U-4020>
2020-12-25 19:34:17 +0900

    bb

--- push.sh
+++ push.sh
@@ -1,3 +1,28 @@
-#!/bin/sh 
-cd /mnt/nas2/entware/share/goldsnake.github.io && git push
+#!/bin/sh
+cd /mnt/nas2/entware/share/goldsnake.github.io
+if [ "$1" = "" ]; then
+
+    echo "Please input  1 argument string \nexmaple: sh gitpush \"add all\""
+
+else
+
+    git init
+
+    git config --global user.name "taekwang.lee"
+
+    git config --global user.email "ltkblue@gmail.com"  
+
+#    git config --global credential.helper cache
+
+#    git remote add origin https://github.com/sunghojang/QT_TCPIP.git
+
+    git add *
+
+    git commit -m "$1"
+
+    git fetch
+
+    git push
+
+fi
 
